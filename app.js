13af8e6cb9ab7bdff2184ef9f64c537cc415c987
Jimin <jiminsung27@gmail.com>
2015-08-05 12:45:31 -0400

    Swig Templating Engine Added

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
 var express = require('express');
-var app = express(); // creates an instance of an express application
 var swig = require('swig');
+var morgan = require('morgan');
+var app = express();
 
-var people = {title: 'An Example', people: [{name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]};
+swig.setDefaults({ cache: false });
 
-swig.renderFile(__dirname + '/views/index.html', people, function (error, output){
-	if (error) throw error;
-	console.log(output);
-})
-
-app.engine('swig', swig.renderFile);
+app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
 
-app.set('views', __dirname + '/views');
-
-app.use(function (request, response, next) {
-    // do your logging here
-    response.send("response Path: "+request.path +"\n" + "Verb: " + request.method + "\nStatus Code: " + response.statusCode);
-    //next();
-});
-
-
+app.set('views', './views');
 
 
 app.get("/",function(request, response){
-//	response.sendStatus(200);
-	response.send("Hello World");
+
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+	response.render( 'index', {title: 'Hall of Fame', people: people} );
+
 });
 
 app.get('/news', function(request, response){
